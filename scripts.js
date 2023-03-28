1fa70946929dec7205e71c207e5d0d067242eec4
Ryan Mertz <ryanmertz3@gmail.com>
2023-03-28 05:34:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 
+  function toggleDropdown(id) {
+    const dropdownContent = document.getElementById(id);
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+  }
+
+  function initDropdowns() {
+    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+
+    dropdownButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        toggleDropdown(button.dataset.target);
+      });
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', initDropdowns);
