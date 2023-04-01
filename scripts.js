a31a64700d828ee8d84b17213f395d5b95e0a41b
Ryan Mertz <ryanmertz3@gmail.com>
2023-03-31 19:55:09 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ window.addEventListener("scroll", () => {
     document.querySelector(".header-content").classList.add("scrolled");
     document.querySelector(".skills").classList.add("scrolled");
     document.querySelector(".degrees").classList.add("scrolled");
+    document.querySelector(".degrees2").classList.add("scrolled");
+    document.querySelector(".main").classList.add("scrolled");
+
   } else {
     document.querySelector(".header-content").classList.remove("scrolled");
     document.querySelector(".skills").classList.remove("scrolled");
     document.querySelector(".degrees").classList.remove("scrolled");
+    document.querySelector(".degrees2").classList.remove("scrolled");
+    document.querySelector(".main").classList.remove("scrolled");
+
   }
 });
